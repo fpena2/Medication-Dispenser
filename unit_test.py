8a0eb4376d28caa8973054f4b3d852fdbe4d6e0e
fpena2 <fpena2@unh.newhaven.edu>
2021-12-12 17:13:07 -0600

    testing

--- unit_test.py
+++ unit_test.py
@@ -1,5 +1,8 @@
+import boto3
+from functools import wraps
 import sys
 import os
+
 sys.path.append(os.path.abspath("./src/"))  # nopep8
 from initMsg import msgController
 
@@ -8,10 +11,10 @@ msg = msgController(bucket)
 
 
 def test():
-    # name = "Schedule.json"
+    name = "Schedule.json"
     # name = "current.json"
-    name = "device.json"
-    msg.setFile("public/"+name, "./msg/"+name)
+    # name = "device.json"
+    msg.setFile("public/" + name, "./msg/archive/" + name)
     # s3 = boto3.client("s3")
     # all_objects = s3.list_objects(Bucket=bucket)
     # for key in all_objects:
